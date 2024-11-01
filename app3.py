29ab66b54110f0571353f83e7c671871e02f9da6
t200056 <gimpo.t200056@ggh.goe.go.kr>
2024-11-01 10:52:43 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -6,26 +6,23 @@ model = joblib.load('linear_regression_model.pkl')
 
 # 만든 모델로 테스트 데이터에 대해 예측하기
 st.title('점수 예측')
-
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
-      st.subheader(' ______________________ ')
+st.subheader(' ______________________ ')
       st.write(' - 기계학습 알고리즘 : 로지스틱 회귀 ')
       st.write(' - 학습 데이터 출처 : https://www.kaggle.com/')
       st.write(' - 총 데이터 건 수: 30건')
       st.write(' - 훈련    데이터 : 21건')
       st.write(' - 테스트 데이터 : 9건')
-with col2:
+col1, col2, col3 = st.columns(3)
+
+with col1:
       st.subheader('2. 데이터 시각화')
       st.image('시각화1.png' )   # 이미지 불러오기
-with col3:
+with col2:
       st.subheader('3. 데이터 시각화')
       st.image('시각화2.png' )   # 이미지 불러오기
-with col4:
+with col3:
       st.subheader('4. 상관관계')
       st.image('시각화3.png')   # 이미지 불러오기
-
 
 st.subheader('3. 예측하기')
 st.write('**** 공부시간을 입력하세요.. 인공지능이 당신의 합격/불합격 분류 결과를 알려드립니다!')
